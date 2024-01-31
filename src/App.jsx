5948524190968f7262ddbfce8df925354b044c36
hodal66 <mhthodol@gmail.com>
2024-01-31 15:53:02 +0200

    Start building Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import MainNavigationBar from "./components/navBar/MainNavigationBar";
 import TopNavigationBar from "./components/navBar/TopNavigationBar";
 import HomePage from "./pages/HomePage";
+import AboutUs from "./pages/AboutUs";
+import CoursesPage from "./pages/CoursesPage";
+import ContactUsPage from "./pages/ContactUsPage";
 
 function App() {
   return (
     <div>
-      <TopNavigationBar />
-      <MainNavigationBar />
-      <HomePage />
+      <BrowserRouter>
+        <TopNavigationBar />
+        <MainNavigationBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contact" element={<AboutUs />} />
+          <Route path="/courses" element={<CoursesPage />} />
+          <Route path="/contact" element={<ContactUsPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/navBar/MainNavigationBar.jsx
+++ src/components/navBar/MainNavigationBar.jsx
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom";
+
 function MainNavigationBar() {
   return (
-    <div className=" bg-gray-light font-medium h-screen hover:cursor-pointer pt-2 pb-2">
+    <div className=" bg-gray-light font-medium hover:cursor-pointer pt-2 pb-2">
       <div className="flex justify-between px-8 bg-white items-center">
         <img src="/images/Logo1.png" alt="" className="w-40 h-20" />
         <ul className="flex gap-8 ">
-          <li>Home</li>
-          <li>About</li>
-          <li>Services</li>
-          <li>Courses</li>
-          <li>ContactUs</li>
-          <li>Gallery</li>
+          <Link to={"/"}>Home</Link>
+          <Link to={"/about"}>About</Link>
+          <Link to={"/about"}>Services</Link>
+          <Link to={"/courses"}>Courses</Link>
+          <Link to={"/contact"}>ContactUs</Link>
+          <Link to={"/gallery"}>Gallery</Link>
         </ul>
         <div>
           <button className="bg-orange py-3 px-6">
